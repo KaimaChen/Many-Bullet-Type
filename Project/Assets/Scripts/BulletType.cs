1f35aa13e0f8cb161be4164ee6f7913f310321b7
Kaima <244241917@qq.com>
2019-03-16 15:21:07 +0800

    first-commit

---
+++ Project/Assets/Scripts/BulletType.cs
@@ -0,0 +1,6 @@
+﻿public enum BulletType
+{
+    Normal, //普通的直线型子弹
+    Grenade, //榴弹（抛物线）
+    Track, //跟踪型
+}
---
+++ Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode mForwardKey = KeyCode.W;
+
+    [SerializeField]
+    private KeyCode mBackwardKey = KeyCode.S;
+
+    [SerializeField]
+    private KeyCode mLeftKey = KeyCode.A;
+
+    [SerializeField]
+    private KeyCode mRightKey = KeyCode.D;
+    
+    [SerializeField]
+    private float mSpeed = 5;
+
+    [SerializeField]
+    private Transform mSpawnPos;
+
+    void Awake()
+    {
+        if (mSpawnPos == null)
+            mSpawnPos = transform.Find("SpawnPos");
+    }
+
+    void Update()
+    {
+        HandleMoving();
+        HandleShooting();
+    }
+
+    void HandleMoving()
+    {
+        float dx = 0;
+        float dz = 0;
+
+        if (Input.GetKey(mLeftKey))
+            dx -= mSpeed * Time.deltaTime;
+        if (Input.GetKey(mRightKey))
+            dx += mSpeed * Time.deltaTime;
+
+        if (Input.GetKey(mForwardKey))
+            dz += mSpeed * Time.deltaTime;
+        if (Input.GetKey(mBackwardKey))
+            dz -= mSpeed * Time.deltaTime;
+
+        Vector3 originPos = transform.position;
+        transform.position = new Vector3(originPos.x + dx, originPos.y, originPos.z + dz);
+    }
+
+    void HandleShooting()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Debug.Log("Shoot");
+        }
+    }
+}
