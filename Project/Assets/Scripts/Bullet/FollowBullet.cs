25eaee8582e835b0e5780ab624f74453f971f945
Kaima <244241917@qq.com>
2019-03-19 00:20:33 +0800

    * NormalBullet, FollowBullet

---
+++ Project/Assets/Scripts/Bullet/FollowBullet.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class FollowBullet : MonoBehaviour
+{
+    private const float kMinDistance = 0.5f; //距离小于此值时视为到达目标
+
+    [SerializeField]
+    private float mSpeed = 15;
+
+    [SerializeField]
+    private float mRemainLifeTime = 1f;
+
+    [SerializeField]
+    private Transform mTarget;
+
+    public void DoStart(Transform target, float lifeTime)
+    {
+        mTarget = target;
+        mRemainLifeTime = lifeTime;
+    }
+
+    void Update()
+    {
+        if (mTarget == null)
+        {
+            DoDestroy();
+            return;
+        }
+
+        Vector3 toTarget = mTarget.position - transform.position;
+        float distance = toTarget.magnitude;
+        if(distance <= kMinDistance)
+        {
+            DoDestroy();
+            return;
+        }
+
+        float deltaTime = Mathf.Min(mRemainLifeTime, Time.deltaTime);
+        Vector3 toTargetDir = toTarget.normalized;
+        float offset = Mathf.Min(distance, mSpeed * deltaTime);
+
+        transform.position = transform.position + toTargetDir * offset;
+        transform.rotation = Quaternion.LookRotation(toTargetDir);
+        
+        mRemainLifeTime -= deltaTime;
+        if (mRemainLifeTime <= 0)
+            DoDestroy();
+    }
+
+    void DoDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
--- Project/Assets/Scripts/UI/ControlPanel.cs
+++ Project/Assets/Scripts/UI/ControlPanel.cs
@@ -1,16 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlPanel : MonoBehaviour
 {
-    void Start()
+    public Button BtnFire;
+    public Dropdown BulletDropdown;
+    public Transform Enemy;
+    
+    void Awake()
     {
-        
+        List<Dropdown.OptionData> optionDatas = new List<Dropdown.OptionData>()
+        {
+            new Dropdown.OptionData("普通子弹"),
+            new Dropdown.OptionData("跟踪子弹"),
+        };
+
+        BulletDropdown.AddOptions(optionDatas);
+
+        BtnFire.onClick.AddListener(OnFire);
+    }
+    
+    void OnFire()
+    {
+        int index = BulletDropdown.value;
+        switch(index)
+        {
+            case 0:
+                LoadBullet("NormalBullet");
+                break;
+            case 1:
+                GameObject go = LoadBullet("FollowBullet");
+                go.GetComponent<FollowBullet>().DoStart(Enemy, 10);
+                break;
+        }
     }
 
-    void Update()
+    GameObject LoadBullet(string name)
     {
-        
+        GameObject prefab = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/" + name + ".prefab");
+        GameObject go = Instantiate(prefab);
+        go.transform.position = Vector3.zero;
+        return go;
     }
 }
